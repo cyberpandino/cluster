50ecd745707687ea545b34a781046ec22dae9844
Costa <mcostamagna00@outlook.it>
2026-02-08 10:35:01 +0100

    chore: server eslint config

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,33 +1,44 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 import stylistic from '@stylistic/eslint-plugin';
 
 export default defineConfig([
   { 
-    files: ["**/*.{js,mjs,cjs}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"], 
+    files: ['**/*.{js,mjs,cjs}'], 
+    plugins: { 
+      js, 
+      '@stylistic': stylistic 
+    }, 
+    extends: ['js/recommended'], 
     languageOptions: { 
       globals: globals.node 
     }, 
     rules: {
-    }
-  },
-  { 
-    files: ["**/*.js"], 
-    languageOptions: { 
-      sourceType: "commonjs" 
-    } 
-  },
-  {
-    files: ["**/*.{js,mjs,cjs}"], 
-    plugins: {
-      '@stylistic': stylistic
-    },
-    rules: {
+      'camelcase': ['warn'],      
+      'no-unused-vars': ['off'],
+      'camelcase': [
+        'error', { 
+          'properties': 'always' 
+        }
+      ],
+      'new-cap': [
+        'error', { 
+          'newIsCap': true, 
+          'capIsNew': false 
+        }
+      ],
+      'id-match': [
+        'error',
+        '^([a-z][a-zA-Z0-9]*|[A-Z][A-Z0-9_]*)$',
+        {
+          'onlyDeclarations': true,
+          'properties': false
+        }
+      ],
 
-      '@stylistic/indent': ['error', 2],
+      '@stylistic/indent': ['warn', 2],
+      '@stylistic/quotes': ['warn', 'single'],
     }
-  }
+  },
 ]);
